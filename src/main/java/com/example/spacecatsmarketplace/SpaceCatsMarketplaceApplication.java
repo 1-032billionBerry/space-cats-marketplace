6066b4904923ad9a038597c1d1600604db191fed
billionberry <bycript20@gmail.com>
2024-12-22 17:18:11 +0200

    init commit

---
+++ src/main/java/com/example/spacecatsmarketplace/SpaceCatsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacecatsmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceCatsMarketplaceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpaceCatsMarketplaceApplication.class, args);
+  }
+
+}
